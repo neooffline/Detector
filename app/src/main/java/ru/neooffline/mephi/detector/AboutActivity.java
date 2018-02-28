c7e0c8629adb192f902a063f759507ff39a0b33e
neooffline <admin@neooffline.ru>
2018-02-28 16:56:18 +0300

    first

---
+++ app/src/main/java/ru/neooffline/mephi/detector/AboutActivity.java
@@ -0,0 +1,14 @@
+package ru.neooffline.mephi.detector;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 26.02.2018.
+ */
+public class AboutActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
---
+++ app/src/main/java/ru/neooffline/mephi/detector/MainActivity.java
@@ -0,0 +1,23 @@
+package ru.neooffline.mephi.detector;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        View.OnClickListener settingsPage = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        };
+
+    }
+}
