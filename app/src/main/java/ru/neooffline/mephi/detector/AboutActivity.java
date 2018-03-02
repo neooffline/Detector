5da30bd7e8f31f81b3483e63627d88844b031dbd
neooffline <admin@neooffline.ru>
2018-03-02 15:46:58 +0300

    "2 Activities"

--- app/src/main/java/ru/neooffline/mephi/detector/AboutActivity.java
+++ app/src/main/java/ru/neooffline/mephi/detector/AboutActivity.java
@@ -1,19 +1,18 @@
 package ru.neooffline.mephi.detector;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-/**
- * Created by user on 26.02.2018.
- */
 public class AboutActivity extends Activity {
+    @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
-    public void sumTwoNumbers (View view){
-        Integer a = 15, b=16;
-        Integer c = a + b;
+    public void backToMain (View view){
+        Intent intent2 = new Intent(this, MainActivity.class);
+        startActivity(intent2);
     }
 }
--- app/src/main/java/ru/neooffline/mephi/detector/MainActivity.java
+++ app/src/main/java/ru/neooffline/mephi/detector/MainActivity.java
@@ -6,21 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        View.OnClickListener settingsPage;
-        settingsPage = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
-                startActivity(intent);
-            }
-        };
-        public void sumToom (View view){
-            Integer c = 1+2;
         }
+    public void settingsCall (View view){
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
     }
 }
