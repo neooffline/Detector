6922dd42891efc3df744c2c9c25ddf8b70c04a8c
neooffline <admin@neooffline.ru>
2018-03-06 16:47:04 +0300

    "test Activities"

--- app/src/main/java/ru/neooffline/mephi/detector/MainActivity.java
+++ app/src/main/java/ru/neooffline/mephi/detector/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -18,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
     public void sameActivity (View view){
         Intent toSameActivity = new Intent(this, SameActivity.class);
         startActivity(toSameActivity);
+    }
+    protected void textFill (View view) {
+        Intent change_uDat = new Intent(this, MainActivity.class);
+        startActivity(change_uDat);
+        Integer a = 15;
+        Integer b = a + 5;
+        TextView change_uDat_var = (TextView) findViewById(R.id.uDat_var);
+        change_uDat_var.setText(b);
+
     }
 }
