3f497818301d0e44dd7cc5740a4f2182e1d01bde
neooffline <admin@neooffline.ru>
2018-03-12 11:49:32 +0300

    jamod

--- app/src/main/java/ru/neooffline/mephi/detector/MainActivity.java
+++ app/src/main/java/ru/neooffline/mephi/detector/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.autofill.AutofillValue;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
